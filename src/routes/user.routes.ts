44a2fa6fbfd974b10fcf02b36de2ef0e1747f08f
Felipe Contreras <confedev@gmail.com>
2021-12-11 12:32:40 -0300

    create user

--- src/routes/user.routes.ts
+++ src/routes/user.routes.ts
@@ -1,5 +1,8 @@
 import { Router } from "express";
+import { createUser } from "../controllers/user.controller";
 
 const router = Router();
+
+router.post('/users', createUser);
 
 export default router;
